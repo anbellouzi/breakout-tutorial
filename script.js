a23c68ee6d308ade197fb7748c326abb7ddbcfd6
Anas Bellouzi <anbellouzi@gmail.com>
2021-02-03 00:24:47 -0500

    arrow functions

--- script.js
+++ script.js
@@ -28,28 +28,29 @@ for (let c = 0; c < brickColumnCount; c += 1) {
   }
 }
 
-function keyDownHandler(e) {
+document.addEventListener('keydown', (e) => {
   if (e.key === 'Right' || e.key === 'ArrowRight') {
     rightPressed = true;
   } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
     leftPressed = true;
   }
-}
+}, false);
 
-function keyUpHandler(e) {
+document.addEventListener('keyup', (e) => {
   if (e.key === 'Right' || e.key === 'ArrowRight') {
     rightPressed = false;
   } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
     leftPressed = false;
   }
-}
+}, false);
 
-function mouseMoveHandler(e) {
+document.addEventListener('mousemove', (e) => {
   const relativeX = e.clientX - canvas.offsetLeft;
   if (relativeX > 0 && relativeX < canvas.width) {
     paddleX = relativeX - paddleWidth / 2;
   }
-}
+}, false);
+
 function collisionDetection() {
   for (let c = 0; c < brickColumnCount; c += 1) {
     for (let r = 0; r < brickRowCount; r += 1) {
@@ -69,8 +70,6 @@ function collisionDetection() {
   }
 }
 
-document.addEventListener('keydown', keyDownHandler, false);
-document.addEventListener('keyup', keyUpHandler, false);
 document.addEventListener('mousemove', mouseMoveHandler, false);
 
 function drawBall() {
